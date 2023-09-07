15df2f2de1d6c866cb948b9696a97b7c5edd3803
Tiquillo <ldelgado@gmail.com>
2023-09-07 11:36:12 -0600

    agragadas las funciones asin, atan y asin

--- funtras.py
+++ funtras.py
@@ -5,6 +5,7 @@ iterMax = 2500
 pi_t = 3.14159265358979323846
 eps = 2.2204e-16
 
+
 # qué pasa si el número es mayor a iterMax?
 def factorial_t(x):
     """
@@ -135,7 +136,7 @@ def tan_t(x):
     Entradas: x es el ángulo en radianes
     Salidas: tan(x)
     Restricciones: x debe ser un número real diferente de pi/2"""
-    if (cos_t(x)[0] < tol):
+    if cos_t(x)[0] < tol:
         return [-1, False, "Division by zero", 0]
     else:
         return [sin_t(x)[0] * div_t(cos_t(x)[0])[0], True, "Success", 0]
@@ -179,8 +180,10 @@ def power_t(x, y):
     """
     return x**y
 
+
 def sinh_t(x):
     pass
+
 
 def cosh_t(x):
     pass
@@ -213,12 +216,12 @@ def root_t(x, y):
         return [-1, False, "Division by zero", 0]
     elif y == 1:
         return [x, True, "Success", 0]
-    
+
     act = 0
     prev = x * 0.5
 
     for n in range(iterMax):
-        act = prev - (power_t(prev,y) - x) * div_t(y * power_t(prev, y - 1))[0]
+        act = prev - (power_t(prev, y) - x) * div_t(y * power_t(prev, y - 1))[0]
 
         if abs(act - prev) < tol * act:
             return [act, True, "Success", n]
@@ -227,16 +230,90 @@ def root_t(x, y):
         return [act, True, "Iteration limit reached", iterMax]
 
 
-def asin_t(x):
-    pass
+def asin_t(a):
+    """
+    Calcula el valor de asin(a)
+    Entradas: a es el número del seno inverso
+    Salidas: asin(a)
+    Restricciones: a debe ser un número real entre -1 y 1
+    """
+    act = 0
+    prev = 0
+
+    if a < -1 or a > 1:
+        return [-1, False, "Input must be between -1 and 1", 0]
+
+    if a == 1:
+        return [pi_t / 2, True, "Success", 0]
+    elif a == -1:
+        return [-pi_t / 2, True, "Success", 0]
+
+    for n in range(iterMax):
+        act += (
+            factorial_t(2 * n)
+            * div_t(power_t(4, n) * power_t(factorial_t(n), 2) * (2 * n + 1))[0]
+            * power_t(a, 2 * n + 1)
+        )
+
+        if abs(act - prev) < tol:
+            return [act, True, "Success", n]
+        prev = act
+    else:
+        return [act, True, "Iteration limit reached", iterMax]
 
 
-def atan_t(x):
-    pass
+def atan_t(a):
+    """
+    Calcula el valor de atan(a)
+    Entradas: a es el número del tangente inverso
+    Salidas: atan(a)
+    Restricciones: a debe ser un número real
+    """
+    act = 0
+    prev = 0
+
+    if a == 1:
+        return [pi_t * 0.25, True, "Success", 0]
+    elif a == -1:
+        return [-pi_t * 0.25, True, "Success", 0]
+
+    if a > -1 and a < 1:
+        for n in range(iterMax):
+            act += power_t(-1, n) * power_t(a, 2 * n + 1) * div_t(2 * n + 1)[0]
+
+            if abs(act - prev) < tol:
+                return [act, True, "Success", n]
+            prev = act
+        else:
+            return [act, True, "Iteration limit reached", iterMax]
+
+    else:
+        for n in range(iterMax):
+            act += power_t(-1, n) * div_t((2*n+1)*power_t(a, 2*n+1))[0]
+
+            if abs(act - prev) < tol:
+                if a > 1:
+                    return [pi_t * 0.5 - act, True, "Success", n]
+                else:
+                    return [-pi_t * 0.5 - act, True, "Success", n]
+            prev = act
+        
+        else:
+            if a > 1:
+                return [pi_t * 0.5 - act, True, "Iteration limit reached", iterMax]
+            else:
+                return [-pi_t * 0.5 - act, True, "Iteration limit reached", iterMax]
+
 
 
 def acos_t(x):
-    pass
+    """
+    Calcula el valor de acos(x)
+    Entradas: x es el número del coseno inverso
+    Salidas: acos(x)
+    Restricciones: x debe ser un número real entre -1 y 1
+    """
+    return pi_t * 0.5 - asin_t(x)[0]
 
 
 def sec_t(x):
@@ -281,7 +358,7 @@ print("")
 print("sin_t")
 print("sen 0: ", sin_t(0))
 print("sen pi: ", sin_t(pi_t))
-print("sen pi/4: ", sin_t(pi_t/4))
+print("sen pi/4: ", sin_t(pi_t / 4))
 print("sen 180: ", sin_t(180))
 print("sen 270: ", sin_t(270))
 print("sen 360: ", sin_t(360))
@@ -291,14 +368,14 @@ print("")
 print("cos_t")
 print("cos 0: ", cos_t(0))
 print("cos pi: ", cos_t(pi_t))
-print("cos pi/2: ", cos_t(pi_t/2))
-print("cos pi/4: ", cos_t(pi_t/4))
+print("cos pi/2: ", cos_t(pi_t / 2))
+print("cos pi/4: ", cos_t(pi_t / 4))
 
 print("")
 print("tan_t")
 print("tan 0: ", tan_t(0))
 print("tan pi: ", tan_t(pi_t))
-print("tan pi/4: ", tan_t(pi_t/4))
+print("tan pi/4: ", tan_t(pi_t / 4))
 print("tan 180: ", tan_t(180))
 print("tan 270: ", tan_t(270))
 
@@ -340,6 +417,11 @@ print("tanh_t")
 
 print("")
 print("sqrt_t")
+print("sqrt 0: ", sqrt_t(0))
+print("sqrt 1: ", sqrt_t(1))
+print("sqrt 2: ", sqrt_t(2))
+print("sqrt 10: ", sqrt_t(10))
+print("sqrt 25: ", sqrt_t(25))
 
 print("")
 print("root_t")
@@ -349,6 +431,20 @@ print("root 4, 2: ", root_t(4, 2))
 
 print("")
 print("asin_t")
+print("asin 0: ", asin_t(0))
+print("asin 1: ", asin_t(1))
+print("asin -0.707193123: ", asin_t(-0.707193123))
+print("asin 0.707193123: ", asin_t(0.707193123))
+print("asin 0.5: ", asin_t(0.5))
+print("asin 0.25: ", asin_t(0.25))
+print("asin 0.75: ", asin_t(0.75))
+print("asin 0.125: ", asin_t(0.125))
+print("asin 0.875: ", asin_t(0.875))
+print("asin 0.0625: ", asin_t(0.0625))
+print("asin -0.9375: ", asin_t(-0.9375))
+print("asin 0.03125: ", asin_t(0.03125))
+print("asin 0.96875: ", asin_t(0.96875))
+print("asin 0.015625: ", asin_t(0.015625))
 
 print("")
 print("atan_t")
